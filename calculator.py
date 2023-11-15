8d8043039e1ac42ca6d40f119955921a85bd9c04
harisankar <harisankaratatat@gmail.com>
2023-11-15 21:49:48 +0530

    j

---
+++ calculator.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('calculator.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        expression = request.form['expression']
+        try:
+            result = eval(expression)
+            return render_template('calculator.html', result=result)
+        except Exception as e:
+            return render_template('calculator.html', error=str(e))
+
+if __name__ == '__main__':
+    app.run(debug=True)
